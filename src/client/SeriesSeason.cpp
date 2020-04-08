ccd7ba11b7220e7a1eb1943e81d74728cc48ddd1
akclifto <akclifto@asu.edu>
2020-04-08 15:36:03 -0700

    format series

--- src/client/SeriesSeason.cpp
+++ src/client/SeriesSeason.cpp
@@ -244,6 +244,11 @@ Json::Value SeriesSeason::toJson()
    return jsonLib;
 }
 
+void SeriesSeason::fromJson(Json::Value json)
+{
+   //TODO
+}
+
 void SeriesSeason::setValues(string aTitle, string aSeriesSeason,
                              string aRating, string aGenre,
                              string poster, string plotSummary)
